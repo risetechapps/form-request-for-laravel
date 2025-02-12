85c892dfe0ea90dda9c9575f04fdf6dee9ab0a9d
risetechapps <apps@risetech.com.br>
2025-02-12 10:05:58 -0300

    Corrigido parametros

--- src/FormRequestServiceProvider.php
+++ src/FormRequestServiceProvider.php
@@ -38,9 +38,7 @@ class FormRequestServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if (file_exists(base_path('config/rules.php'))) {
-            $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'rules');
-        }
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'rules');
 
         $this->app->singleton(Commands\MigrateCommand::class, function ($app) {
             return new Commands\MigrateCommand($app['migrator'], $app['events']);
@@ -60,7 +58,7 @@ class FormRequestServiceProvider extends ServiceProvider
 
         foreach ($validatorConfig as $rule => $className) {
 
-            if(new $className() instanceof ValidatorContract){
+            if (new $className() instanceof ValidatorContract) {
                 Validator::extend($rule, function ($attribute, $value, $parameters, $validator) use ($className) {
                     return $className::validate($attribute, $value, $parameters, $validator);
                 });
@@ -69,7 +67,7 @@ class FormRequestServiceProvider extends ServiceProvider
 
         foreach ($validatorDefault as $rule => $className) {
 
-            if(new $className() instanceof ValidatorContract){
+            if (new $className() instanceof ValidatorContract) {
                 Validator::extend($rule, function ($attribute, $value, $parameters, $validator) use ($className) {
                     return $className::validate($attribute, $value, $parameters, $validator);
                 });
