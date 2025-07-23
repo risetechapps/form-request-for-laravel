3a28ef9acba34359840d3eabdce22b54f6d39b12
risetechapps <apps@risetech.com.br>
2025-07-23 14:23:25 -0300

    Corrigido validação de form request.

--- src/Services/Validator/validateUniqueJson.php
+++ src/Services/Validator/validateUniqueJson.php
@@ -14,15 +14,24 @@ class validateUniqueJson implements ValidatorContract
         try {
 
             $table = $parameters[0];
-            [$jsonPath, $valueId] = explode(':', $parameters[1]);
-            [$jsonField, $jsonKey] = explode('.', $jsonPath);
 
+            $secondItem = $parameters[1];
+
+            $jsonPath = $secondItem;
+            $valueId = null;
+
+            if (str_contains($secondItem, ':')) {
+                [$jsonPath, $valueId] = explode(':', $secondItem, 2);
+            }
+
+            [$jsonField, $jsonKey] = explode('.', $jsonPath);
 
             return DB::table($table)
                     ->where("{$jsonField}->{$jsonKey}", $value)
                     ->where('id', '!=', $valueId)
                     ->count() === 0;
         } catch (\Exception $exception) {
+
             logglyError()->exception($exception)
                 ->withProperties(['attribute' => $attribute, 'value' => $value, 'parameters' => $parameters])
                 ->log("Error validating data rules");
