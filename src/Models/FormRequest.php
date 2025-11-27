b5c8c0a61c4678c3d2e3639b74e5fb7aa52078a8
risetechapps <apps@risetech.com.br>
2025-11-27 17:23:09 -0300

    Corrigido imports

--- src/Models/FormRequest.php
+++ src/Models/FormRequest.php
@@ -5,15 +5,14 @@ namespace RiseTechApps\FormRequest\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
-use RiseTechApps\HasUuid\Traits\HasUuid\HasUuid;
-use RiseTechApps\Monitoring\Traits\HasLoggly\HasLoggly;
+use RiseTechApps\HasUuid\Traits\HasUuid;
 
 /**
  * Modelo Eloquent que representa definições de formulários dinâmicos persistidos.
  */
 class FormRequest extends Model
 {
-    use HasFactory, Notifiable, HasUuid, HasLoggly;
+    use HasFactory, Notifiable, HasUuid;//, HasLoggly;
 
     /**
      * @var list<string>
