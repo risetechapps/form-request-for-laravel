8a4263c29c07849384ce5b63810f6ec6ff68cc94
risetechapps <apps@risetech.com.br>
2026-04-21 14:24:38 -0300

    Implementado suporte a mensagem

--- src/Contracts/RulesContract.php
+++ src/Contracts/RulesContract.php
@@ -4,7 +4,24 @@ namespace RiseTechApps\FormRequest\Contracts;
 
 interface RulesContract
 {
+    /**
+     * Retorna as regras de validação.
+     *
+     * @return array<string, array<string, mixed>>
+     */
     public static function Rules(): array;
 
+    /**
+     * Retorna as mensagens de validação personalizadas.
+     *
+     * @return array<string, array<string, string>>
+     */
+    public static function Messages(): array;
+
+    /**
+     * Retorna validadores customizados.
+     *
+     * @return array<string, string>
+     */
     public static function Validator(): array;
 }
