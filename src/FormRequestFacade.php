3bac9e9ac8f1ebda8d9e81151f154bc92ded3508
risetechapps <apps@risetech.com.br>
2026-07-20 15:42:52 -0300

    Implementado melhorias para a versão do php8.4

--- src/FormRequestFacade.php
+++ src/FormRequestFacade.php
@@ -5,6 +5,14 @@ namespace RiseTechApps\FormRequest;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static void routes(array $options = [])
+ * @method static void register(string $name, array $rules, array $messages = [], array $metadata = [])
+ * @method static void forget(string $name)
+ * @method static array resolve(string $name, array $context = [])
+ * @method static void presenceScope(string $table, callable $scope, ?string $name = null)
+ * @method static void presenceScopeAll(callable $scope, ?string $name = null)
+ * @method static mixed withoutPresenceScopes(callable $callback)
+ *
  * @see \RiseTechApps\FormRequest\FormRequest
  */
 class FormRequestFacade extends Facade
